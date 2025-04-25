6ed8be4eeab4f978b128c7e491ca4d6a540def4b
eungikim <eungikim.me@gmail.com>
2025-04-25 18:36:27 +0900

    section12: 단일 트랜잭션화
    Transaction 어노테이션을 추가하거나 주석처리한 부분이 있음

--- springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -55,6 +55,24 @@ class MemberServiceTest {
     }
 
 
+    /**
+     * memberService    @Transactional: ON
+     * memberRepository @Transactional: OFF
+     * logRepository    @Transactional: OFF
+     */
+    @Test
+    void singleTx() {
+        //given
+        String username = "singleTx";
+
+        //when
+        memberService.joinV1(username);
+
+        //then: 모든 데이터가 정상 저장된다.
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertTrue(logRepository.find(username).isPresent());
+    }
+
 
 
 }
